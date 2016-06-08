a78081561672edb731d9ddb07c2c5d3f0a47d8d0
victsomie <victsomie@yahoo.com>
2016-06-08 12:15:20 +0300

    added files with extra fragment

--- app/src/main/java/com/example/victor/fragments/BottomSectionFragment.java
+++ app/src/main/java/com/example/victor/fragments/BottomSectionFragment.java
@@ -8,19 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
-
-public class TopSectionFragment extends Fragment{ //Makes this inherit from fragment
-
-
-   @Nullable
+public class BottomSectionFragment extends Fragment{
 
     //overide onCreateView
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-       View view = inflater.inflate(R.layout.top_section_fragment, container, false);
-       return view;
+        View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+        return view;
     }
 }
